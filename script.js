1d4e767b32e7496d0fce4afe33fc4f25df78a54e
parjana94 <76865871+parjana94@users.noreply.github.com>
2022-11-18 19:28:04 +0400

    login

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ let emailField = document.getElementById("email-field");
 let message = document.querySelector(".validation");
 let emailSec = document.getElementById("validation-sec");
 let emailFieldSec = document.getElementById("email-field-sec");
+const signIn = document.querySelector("#signin");
+const loginBlock = document.querySelector(".login");
+const container = document.querySelector(".container");
+const close = document.querySelector("#close");
+const btn = document.querySelector("#btn");
+const mail = document.querySelector("#mail-input");
+const pass = document.querySelector(".pass");
 
 function validateEmail() {
   if (emailField.value.match(pattern)) {
@@ -28,3 +35,29 @@ function validateEmailSec() {
     return false;
   }
 }
+
+signIn.addEventListener("click", function () {
+  loginBlock.style.display = "block";
+});
+
+close.addEventListener("click", function () {
+  loginBlock.style.display = "none";
+  mail.value = "";
+  pass.value = "";
+  document.querySelector("#mail-error").style.display = "none";
+  document.querySelector("#pass-error").style.display = "none";
+});
+
+btn.addEventListener("click", function () {
+  if (mail.value && pass.value) {
+    loginBlock.style.display = "none";
+    mail.value = "";
+    pass.value = "";
+    document.querySelector("#mail-error").style.display = "none";
+    document.querySelector("#pass-error").style.display = "none";
+  } else if (!mail.value) {
+    document.querySelector("#mail-error").style.display = "block";
+  } else if (!pass.value) {
+    document.querySelector("#pass-error").style.display = "block";
+  }
+});
